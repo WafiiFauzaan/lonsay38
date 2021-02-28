235c80060664df3247a195b1d76a0a44d889f0e7
WafiiFauzaan <68062653+WafiiFauzaan@users.noreply.github.com>
2021-02-28 12:39:04 +0700

    Add files via upload

--- lonsay38.sql
+++ lonsay38.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.4
+-- version 4.8.5
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 06 Feb 2021 pada 20.48
--- Versi server: 10.4.16-MariaDB
--- Versi PHP: 7.3.24
+-- Generation Time: Feb 28, 2021 at 06:32 AM
+-- Server version: 10.1.38-MariaDB
+-- PHP Version: 7.3.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -24,55 +25,77 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `bahan_baku`
+-- Table structure for table `bahan_baku`
 --
 
 CREATE TABLE `bahan_baku` (
-  `id_bahan_baku` int(10) NOT NULL,
-  `id_satuan` int(10) NOT NULL,
-  `nama_bahan_baku` int(10) DEFAULT NULL,
-  `harga_bahan_baku` int(10) DEFAULT NULL,
-  `Column` int(10) DEFAULT NULL,
-  `userid_user` int(10) NOT NULL
+  `id` int(10) NOT NULL,
+  `satuan_bahan_baku_id` int(10) NOT NULL,
+  `nama` varchar(255) DEFAULT NULL,
+  `harga` double DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `bahan_baku`
+--
+
+INSERT INTO `bahan_baku` (`id`, `satuan_bahan_baku_id`, `nama`, `harga`) VALUES
+(1, 2, 'Tepung', 10000);
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `penjualan`
+-- Table structure for table `penjualan`
 --
 
 CREATE TABLE `penjualan` (
-  `id_penjualan` int(10) NOT NULL,
-  `id_user` int(10) NOT NULL,
+  `id` int(10) NOT NULL,
+  `user_id` int(10) NOT NULL,
   `waktu_transaksi` timestamp NULL DEFAULT NULL,
-  `nama_pelanggan` varchar(255) DEFAULT NULL
+  `last_update` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `nama` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `penjualan`
+--
+
+INSERT INTO `penjualan` (`id`, `user_id`, `waktu_transaksi`, `last_update`, `nama`) VALUES
+(5, 3, '2021-02-26 17:00:00', '2021-02-24 03:15:52', 'Joni'),
+(7, 3, '2021-02-24 03:09:07', '2021-02-24 03:15:52', 'Wafii');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `produk`
+-- Table structure for table `produk`
 --
 
 CREATE TABLE `produk` (
-  `id_produk` int(10) NOT NULL,
-  `id_tipe_produk` int(10) NOT NULL,
-  `id_user` int(10) NOT NULL,
-  `nama_produk` varchar(50) DEFAULT NULL,
+  `id` int(10) NOT NULL,
+  `tipe_produk_id` int(10) NOT NULL,
+  `user_id` int(10) NOT NULL,
+  `nama` varchar(50) DEFAULT NULL,
   `harga_produk` double DEFAULT NULL,
   `harga_produksi` double DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `produk`
+--
+
+INSERT INTO `produk` (`id`, `tipe_produk_id`, `user_id`, `nama`, `harga_produk`, `harga_produksi`) VALUES
+(3, 3, 4, 'Roti 38', 5000, NULL);
+
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `produk_bahan_baku`
+-- Table structure for table `produk_bahan_baku`
 --
 
 CREATE TABLE `produk_bahan_baku` (
-  `id_produk` int(10) NOT NULL,
-  `id_bahan_baku` int(10) NOT NULL,
+  `id` int(10) NOT NULL,
+  `produk_id` int(10) NOT NULL,
+  `bahan_baku_id` int(10) NOT NULL,
   `berat_bahan_baku` double DEFAULT NULL,
   `harga_bahan_baku` double DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -80,13 +103,13 @@ CREATE TABLE `produk_bahan_baku` (
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `produk_penjualan`
+-- Table structure for table `produk_penjualan`
 --
 
 CREATE TABLE `produk_penjualan` (
-  `id_produk_penjualan` int(10) NOT NULL,
-  `id_produk` int(10) NOT NULL,
-  `id_penjualan` int(10) NOT NULL,
+  `id` int(10) NOT NULL,
+  `produk_id` int(10) NOT NULL,
+  `penjualan_id` int(10) NOT NULL,
   `jumlah_produk` double DEFAULT NULL,
   `harga_produk` double DEFAULT NULL,
   `sub_total` double DEFAULT NULL,
@@ -97,42 +120,59 @@ CREATE TABLE `produk_penjualan` (
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `produk_penjualan_bahan_baku`
+-- Table structure for table `produk_penjualan_bahan_baku`
 --
 
 CREATE TABLE `produk_penjualan_bahan_baku` (
-  `id_produk_penjualan` int(10) NOT NULL,
-  `id_bahan_baku` int(10) NOT NULL,
-  `berat_bahan_baku` int(10) DEFAULT NULL,
-  `harga_bahan_baku` int(10) DEFAULT NULL
+  `bahan_baku_id` int(10) NOT NULL,
+  `produk_penjualan_id` int(10) NOT NULL,
+  `berat_bahan_baku` double DEFAULT NULL,
+  `harga_bahan_baku` double DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `satuan_bahan_baku`
+-- Table structure for table `satuan_bahan_baku`
 --
 
 CREATE TABLE `satuan_bahan_baku` (
-  `id_satuan` int(10) NOT NULL,
-  `nama_satuan` varchar(50) DEFAULT NULL
+  `id` int(10) NOT NULL,
+  `nama` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `satuan_bahan_baku`
+--
+
+INSERT INTO `satuan_bahan_baku` (`id`, `nama`) VALUES
+(1, 'Gram'),
+(2, 'KG');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `tipe_produk`
+-- Table structure for table `tipe_produk`
 --
 
 CREATE TABLE `tipe_produk` (
-  `id_tipe_produk` int(10) NOT NULL,
-  `nama_tipe_produk` varchar(255) DEFAULT NULL
+  `id` int(10) NOT NULL,
+  `nama` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tipe_produk`
+--
+
+INSERT INTO `tipe_produk` (`id`, `nama`) VALUES
+(3, 'Ceminal'),
+(2, 'Makanan'),
+(1, 'Minuman');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `user`
+-- Table structure for table `user`
 --
 
 CREATE TABLE `user` (
@@ -142,178 +182,185 @@ CREATE TABLE `user` (
   `password` varchar(255) DEFAULT NULL,
   `nama` varchar(50) DEFAULT NULL,
   `alamat` varchar(50) DEFAULT NULL,
-  `no_handphone` varchar(15) DEFAULT NULL
+  `no_handphone` varchar(25) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `user`
+-- Dumping data for table `user`
 --
 
 INSERT INTO `user` (`id`, `type`, `username`, `password`, `nama`, `alamat`, `no_handphone`) VALUES
-(1, 'pemilik', 'wafii', '47b3b4e326df3e1096fe6bdc5ca5ad78', 'Wafii', 'Kosambi Baru', '0'),
-(3, 'admin', 'haekal', 'b73008f47195d9746adc54461fb7ac0d', 'Haekal', 'Bekasi', '878337226'),
-(4, 'inventori', 'fikri', '5d4864249b21de08642aa6ff4178b346', 'Fikri', 'Jakarta', '788899283');
+(1, 'pemilik', 'pemilik', '58399557dae3c60e23c78606771dfa3d', 'Wafii', 'Kosambi', '087778356201'),
+(3, 'admin', 'admin', '21232f297a57a5a743894a0e4a801fc3', 'Haekal', 'Cibubur', NULL),
+(4, 'inventori', 'inventori', '4e943c28c3b011e0540ff9a19334953b', 'Hana', 'Cililin', NULL);
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indeks untuk tabel `bahan_baku`
+-- Indexes for table `bahan_baku`
 --
 ALTER TABLE `bahan_baku`
-  ADD PRIMARY KEY (`id_bahan_baku`),
-  ADD UNIQUE KEY `nama_bahan_baku` (`nama_bahan_baku`),
-  ADD KEY `FKbahan_baku666706` (`id_satuan`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `nama_bahan_baku` (`nama`),
+  ADD KEY `FKbahan_baku888293` (`satuan_bahan_baku_id`);
 
 --
--- Indeks untuk tabel `penjualan`
+-- Indexes for table `penjualan`
 --
 ALTER TABLE `penjualan`
-  ADD PRIMARY KEY (`id_penjualan`),
-  ADD KEY `FKpenjualan985612` (`id_user`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FKpenjualan114403` (`user_id`);
 
 --
--- Indeks untuk tabel `produk`
+-- Indexes for table `produk`
 --
 ALTER TABLE `produk`
-  ADD PRIMARY KEY (`id_produk`),
-  ADD KEY `FKproduk903803` (`id_tipe_produk`),
-  ADD KEY `FKproduk474519` (`id_user`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FKproduk126876` (`tipe_produk_id`),
+  ADD KEY `FKproduk345729` (`user_id`);
 
 --
--- Indeks untuk tabel `produk_bahan_baku`
+-- Indexes for table `produk_bahan_baku`
 --
 ALTER TABLE `produk_bahan_baku`
-  ADD PRIMARY KEY (`id_bahan_baku`),
-  ADD KEY `FKproduk_bah802344` (`id_produk`),
-  ADD KEY `FKproduk_bah959777` (`id_bahan_baku`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FKproduk_bah523683` (`produk_id`),
+  ADD KEY `FKproduk_bah454805` (`bahan_baku_id`);
 
 --
--- Indeks untuk tabel `produk_penjualan`
+-- Indexes for table `produk_penjualan`
 --
 ALTER TABLE `produk_penjualan`
-  ADD PRIMARY KEY (`id_produk_penjualan`),
-  ADD KEY `FKproduk_pen829841` (`id_produk`),
-  ADD KEY `FKproduk_pen923535` (`id_penjualan`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FKproduk_pen815720` (`produk_id`),
+  ADD KEY `FKproduk_pen651989` (`penjualan_id`);
 
 --
--- Indeks untuk tabel `produk_penjualan_bahan_baku`
+-- Indexes for table `produk_penjualan_bahan_baku`
 --
 ALTER TABLE `produk_penjualan_bahan_baku`
-  ADD KEY `FKproduk_pen763937` (`id_produk_penjualan`),
-  ADD KEY `FKproduk_pen153419` (`id_bahan_baku`);
+  ADD PRIMARY KEY (`bahan_baku_id`,`produk_penjualan_id`),
+  ADD KEY `FKproduk_pen966576` (`produk_penjualan_id`),
+  ADD KEY `FKproduk_pen648446` (`bahan_baku_id`);
 
 --
--- Indeks untuk tabel `satuan_bahan_baku`
+-- Indexes for table `satuan_bahan_baku`
 --
 ALTER TABLE `satuan_bahan_baku`
-  ADD PRIMARY KEY (`id_satuan`),
-  ADD UNIQUE KEY `nama_satuan` (`nama_satuan`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `nama` (`nama`);
 
 --
--- Indeks untuk tabel `tipe_produk`
+-- Indexes for table `tipe_produk`
 --
 ALTER TABLE `tipe_produk`
-  ADD PRIMARY KEY (`id_tipe_produk`),
-  ADD UNIQUE KEY `nama_tipe_produk` (`nama_tipe_produk`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `nama` (`nama`);
 
 --
--- Indeks untuk tabel `user`
+-- Indexes for table `user`
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `username` (`username`);
 
 --
--- AUTO_INCREMENT untuk tabel yang dibuang
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT untuk tabel `bahan_baku`
+-- AUTO_INCREMENT for table `bahan_baku`
 --
 ALTER TABLE `bahan_baku`
-  MODIFY `id_bahan_baku` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT untuk tabel `penjualan`
+-- AUTO_INCREMENT for table `penjualan`
 --
 ALTER TABLE `penjualan`
-  MODIFY `id_penjualan` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
--- AUTO_INCREMENT untuk tabel `produk`
+-- AUTO_INCREMENT for table `produk`
 --
 ALTER TABLE `produk`
-  MODIFY `id_produk` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- AUTO_INCREMENT untuk tabel `produk_penjualan`
+-- AUTO_INCREMENT for table `produk_bahan_baku`
+--
+ALTER TABLE `produk_bahan_baku`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `produk_penjualan`
 --
 ALTER TABLE `produk_penjualan`
-  MODIFY `id_produk_penjualan` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT untuk tabel `satuan_bahan_baku`
+-- AUTO_INCREMENT for table `satuan_bahan_baku`
 --
 ALTER TABLE `satuan_bahan_baku`
-  MODIFY `id_satuan` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT untuk tabel `tipe_produk`
+-- AUTO_INCREMENT for table `tipe_produk`
 --
 ALTER TABLE `tipe_produk`
-  MODIFY `id_tipe_produk` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- AUTO_INCREMENT untuk tabel `user`
+-- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
   MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
--- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+-- Constraints for dumped tables
 --
 
 --
--- Ketidakleluasaan untuk tabel `bahan_baku`
+-- Constraints for table `bahan_baku`
 --
 ALTER TABLE `bahan_baku`
-  ADD CONSTRAINT `FKbahan_baku666706` FOREIGN KEY (`id_satuan`) REFERENCES `satuan_bahan_baku` (`id_satuan`);
+  ADD CONSTRAINT `FKbahan_baku888293` FOREIGN KEY (`satuan_bahan_baku_id`) REFERENCES `satuan_bahan_baku` (`id`);
 
 --
--- Ketidakleluasaan untuk tabel `penjualan`
+-- Constraints for table `penjualan`
 --
 ALTER TABLE `penjualan`
-  ADD CONSTRAINT `FKpenjualan985612` FOREIGN KEY (`id_user`) REFERENCES `user` (`id`);
+  ADD CONSTRAINT `FKpenjualan114403` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
 
 --
--- Ketidakleluasaan untuk tabel `produk`
+-- Constraints for table `produk`
 --
 ALTER TABLE `produk`
-  ADD CONSTRAINT `FKproduk474519` FOREIGN KEY (`id_user`) REFERENCES `user` (`id`),
-  ADD CONSTRAINT `FKproduk903803` FOREIGN KEY (`id_tipe_produk`) REFERENCES `tipe_produk` (`id_tipe_produk`);
+  ADD CONSTRAINT `FKproduk126876` FOREIGN KEY (`tipe_produk_id`) REFERENCES `tipe_produk` (`id`),
+  ADD CONSTRAINT `FKproduk345729` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
 
 --
--- Ketidakleluasaan untuk tabel `produk_bahan_baku`
+-- Constraints for table `produk_bahan_baku`
 --
 ALTER TABLE `produk_bahan_baku`
-  ADD CONSTRAINT `FKproduk_bah802344` FOREIGN KEY (`id_produk`) REFERENCES `produk` (`id_produk`),
-  ADD CONSTRAINT `FKproduk_bah959777` FOREIGN KEY (`id_bahan_baku`) REFERENCES `bahan_baku` (`id_bahan_baku`);
+  ADD CONSTRAINT `FKproduk_bah454805` FOREIGN KEY (`bahan_baku_id`) REFERENCES `bahan_baku` (`id`),
+  ADD CONSTRAINT `FKproduk_bah523683` FOREIGN KEY (`produk_id`) REFERENCES `produk` (`id`);
 
 --
--- Ketidakleluasaan untuk tabel `produk_penjualan`
+-- Constraints for table `produk_penjualan`
 --
 ALTER TABLE `produk_penjualan`
-  ADD CONSTRAINT `FKproduk_pen829841` FOREIGN KEY (`id_produk`) REFERENCES `produk` (`id_produk`),
-  ADD CONSTRAINT `FKproduk_pen923535` FOREIGN KEY (`id_penjualan`) REFERENCES `penjualan` (`id_penjualan`);
+  ADD CONSTRAINT `FKproduk_pen651989` FOREIGN KEY (`penjualan_id`) REFERENCES `penjualan` (`id`),
+  ADD CONSTRAINT `FKproduk_pen815720` FOREIGN KEY (`produk_id`) REFERENCES `produk` (`id`);
 
 --
--- Ketidakleluasaan untuk tabel `produk_penjualan_bahan_baku`
+-- Constraints for table `produk_penjualan_bahan_baku`
 --
 ALTER TABLE `produk_penjualan_bahan_baku`
-  ADD CONSTRAINT `FKproduk_pen153419` FOREIGN KEY (`id_bahan_baku`) REFERENCES `bahan_baku` (`id_bahan_baku`),
-  ADD CONSTRAINT `FKproduk_pen763937` FOREIGN KEY (`id_produk_penjualan`) REFERENCES `produk_penjualan` (`id_produk_penjualan`);
+  ADD CONSTRAINT `FKproduk_pen648446` FOREIGN KEY (`bahan_baku_id`) REFERENCES `bahan_baku` (`id`),
+  ADD CONSTRAINT `FKproduk_pen966576` FOREIGN KEY (`produk_penjualan_id`) REFERENCES `produk_penjualan` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
